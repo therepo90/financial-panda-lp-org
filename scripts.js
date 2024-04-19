425f0e6d59b88456a94a9071965ae1c1f73a7816
therepo90 <therepo90@gmail.com>
2024-04-19 12:45:09 +0200

    wrking stuff

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('insiders').onclick = function () {
+    window.location.href = 'https://app.financialpanda.pl/app/insiders';
+};
+document.getElementById('stock-picker').onclick = function () {
+    window.location.href = 'https://app.financialpanda.pl/app/candidates';
+};
