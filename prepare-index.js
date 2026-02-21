99dcd659913fa682b1c9c598b2476d6233b816fd
Tomasz Szepczynski <therepo90@gmail.com>
2026-02-22 00:34:38 +0100

    fsa

--- prepare-index.js
+++ prepare-index.js
@@ -12,7 +12,8 @@ const run = async () => {
     // fs promises read
     const file = await fs.readFile(path, 'utf8');
     // replace https://financialpanda.pl/ with https://financialpanda.org/
-    const newFile = file.replace(/financialpanda\.pl/g, 'financialpanda.org');
+    let newFile = file.replace(/financialpanda\.pl/g, 'financialpanda.org');
+    newFile = newFile.replace(/app\.financialpanda\.org/g, 'app.financialpanda.pl');
     // write the file back
     console.log('writing')
     await fs.writeFile(path, newFile);
